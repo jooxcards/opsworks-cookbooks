2024d6d88cbe435e01c97069c5778543ff33a5f5
Andre Joox <andre@joox.io>
2016-07-19 11:24:59 -0300

    updated

--- deploy/recipes/joox-api.rb
+++ deploy/recipes/joox-api.rb
@@ -1,5 +1,3 @@
-include_recipe 'deploy'
-
 unless File.exists?("/usr/bin/composer")
   system "wget https://getcomposer.org/installer -O /tmp/composer-setup.php && php /tmp/composer-setup.php --force --install-dir=/usr/bin --filename=composer"
 end
